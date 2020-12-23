b0c63d11de538ab81dd0f26a93a1553a3769121b
Guilherme <guilherme.devon@hotmail.com>
2020-12-22 21:14:57 -0300

    Trabalho feito, alegria e comemorações! :)

--- projeto_final/fake.v
+++ projeto_final/fake.v
@@ -1,37 +1,67 @@
 module fake(
     input clock,
-    input signed [7:0] x,
-    input signed [15:0] a,
-    input signed [15:0] b,
-    input signed [15:0] c,
+    input [ 7:0] x,
+    input [15:0] a,
+    input [15:0] b,
+    input [15:0] c,
     input enable,
     input reset,
-    output signed [15:0] result,
+    output [15:0] result,
     output ready,
     output valid
 );
 
-reg [3:0] state;
 
-reg [15:0] temp;
+reg [ 7:0] X;
+reg [15:0] A;
+reg [15:0] B;
+reg [ 3:0] state;
+reg debug;
 
-assign valid = state == 15;
+assign valid = state == 6;
 assign ready = state == 0;
 
-assign result = a * x * x + b * x + c;
+reg soma;
+assign result = soma;
 
-always @(posedge clock or reset)
-begin
-    if (reset) begin
-        state <= 0;
-    end
+always @(posedge clock) begin
+    if (reset == 1) 
+        debug <= 1;  
+
     else begin
         if (state == 0 && ~enable)
-            state <= state;
-        else 
-            state <= state + 1;
+            state <= state;  
+
+            else begin
+                case(state)
+                0: begin
+                   state = 1;
+                end
+                1: begin
+                    X <= x*x;
+                    state = 2;
+                end
+                2: begin
+                   A <= a*X;
+                   state = 3;
+                end
+                3: begin
+                   B <= b*x;
+                   state = 4;
+                end
+                4: begin
+                   soma <= B+A;    
+                   state = 5;
+                end
+                5: begin
+                   soma <= soma+c;
+                   state = 6;
+                end
+                6: begin
+                   state = 0;
+                end        
+            endcase
+        end
     end
 end
-
-
 endmodule
